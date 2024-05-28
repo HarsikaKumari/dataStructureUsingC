15a751729d1461ec6202b75ecb695f3e5a824ab6
Harsika Kumari <kumariharsika8@gmail.com>
2024-05-28 12:28:09 +0530

    inprovement in code in insertion in array

--- array/insertion.c
+++ array/insertion.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-int pos, value, size;
 
 int main() {
+    int pos, value, size;
     printf("Enter the size of the array you want: ");
     scanf("%d", &size);
     int arr[size];
@@ -16,16 +16,15 @@ int main() {
     printf("Enter the value which you want to insert in that position:");
     scanf("%d", &value);
 
-    for(int i = size-1; i > pos-1; i--){
-        arr[i-1] = arr[i];
+    for(int i = size; i > pos; i--){
+        arr[i] = arr[i-1];
     }
-    arr[pos-1] = value;
-    size++;
+    arr[pos] = value;
 
     printf("The new array after inserting your value is: ");
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i <= size; i++)
     {
-        printf("arr[%d]", arr[i]);
+        printf("%d ", arr[i]);
     }
     
     return 0;
