a313f63e1549bb39b4e2c08337bd118c9001950a
Harsika Kumari <kumariharsika8@gmail.com>
2024-08-12 23:21:56 +0530

    linear search in array

---
+++ array/linearSrchArr.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// Function to perform linear search
+int linearSearch(int arr[], int n, int target) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            return i; // Return the index if found
+        }
+    }
+    return -1; // Return -1 if not found
+}
+
+int main() {
+    int arr[] = {5, 3, 8, 1, 9, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 8;
+
+    int result = linearSearch(arr, n, target);
+    if (result != -1) {
+        printf("Element %d found at index %d\n", target, result);
+    } else {
+        printf("Element %d not found in the array\n", target);
+    }
+
+    return 0;
+}
