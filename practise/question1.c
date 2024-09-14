7f037fb383ad9603f73d570b50ed6be465ddc56a
Harsika Kumari <kumariharsika8@gmail.com>
2024-09-14 21:58:19 +0530

    practising some qustions in C

--- practise/question1.c
+++ practise/question1.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
 
+ int sum(int x) {
+    printf("x is %d ", x);
+        x++;
+    }
+
 void main() {
     int x = 97;
     int y = sizeof(x++);
     printf("x is %d ", x);
     printf("y is %d", y);
+
     return 0;
 }
